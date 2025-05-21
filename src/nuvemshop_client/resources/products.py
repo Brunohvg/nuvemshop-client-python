286d472fca086ca4bce4eec0c0751bfcb4c1d1d9
Bruno Vidal <brunovidal27.19@gmail.com>
2025-05-21 07:41:21 -0300

    3

--- src/nuvemshop_client/resources/products.py
+++ src/nuvemshop_client/resources/products.py
@@ -1,26 +1,28 @@
 # src/nuvemshop_client/resources/products.py
 
+from typing import Union
+
 class Products:
     def __init__(self, client, store_id: int):
         self.client = client
         self.store_id = store_id
 
-    def list(self, page: int = 1, limit: int = 50) -> dict:
-        path = f'/stores/{self.store_id}/products'
+    def list(self, page: int = 1, limit: int = 50) -> Union[dict, list]:
+        path = f'stores/{self.store_id}/products'
         return self.client.get(path, params={'page': page, 'limit': limit})
 
     def get(self, product_id: int) -> dict:
-        path = f'/stores/{self.store_id}/products/{product_id}'
+        path = f'stores/{self.store_id}/products/{product_id}'
         return self.client.get(path)
 
     def create(self, data: dict) -> dict:
-        path = f'/stores/{self.store_id}/products'
+        path = f'stores/{self.store_id}/products'
         return self.client.post(path, data=data)
 
     def update(self, product_id: int, data: dict) -> dict:
-        path = f'/stores/{self.store_id}/products/{product_id}'
+        path = f'stores/{self.store_id}/products/{product_id}'
         return self.client.put(path, data=data)
 
     def delete(self, product_id: int) -> None:
-        path = f'/stores/{self.store_id}/products/{product_id}'
+        path = f'stores/{self.store_id}/products/{product_id}'
         self.client.delete(path)
